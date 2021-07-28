cc0aa8bdeeef1eedce16a61f1f220a8eeb464744
nguyenvukhang <brew4k@gmail.com>
2021-07-28 15:10:01 +0800

    wrote plan for up to when I obtain user-centric data

--- design.js
+++ design.js
@@ -22,6 +22,66 @@ const user_metadata = (team_metadata_spreadsheet_id) => {
   return {}
 }
 
+const user_run_data = (user_metadata) => {
+  const user_active_spreadsheet_ids = (sheets.js, user_metadata) => {
+    /*
+     * search sheets.js for sheet ids that match
+     *  - username
+     *  - type: run
+     * for each match, save the spreadsheet id
+     * return an object whose root props are the spreadsheet ids
+     */
+    return {}
+  }
+  const result = (user_active_spreadsheet_ids) => {
+    /*
+     * for each spreadsheet id,
+     * send a request for data and save it under the corresponding key
+     * return an object:
+     *   with level 1 props as spreadsheet id,
+     *   and level 2 props as sheet title
+     */
+    return {}
+  }
+  return { result }
+}
+
+const user_run_distance_data = (user_run_data) => {
+  /*
+   * data fields: 
+   *  - distance
+   *  - timing
+   *  - average pace
+   *  - date
+   * extras:
+   *  - personal best (>=1, >=2.4, >=5)
+   */
+}
+
+const user_run_interval_data = (user_run_data) => {
+  /*
+   * data fields: 
+   *  - sets
+   *  - distances
+   *  - start_interval
+   *  - timings
+   *  - date
+   * extras:
+   *  - personal best (400, 800, 1200, 1600)
+   */
+}
+
+const user_run_onoff_data = (user_run_data) => {
+  /*
+   * data fields: 
+   *  - sets
+   *  - on times
+   *  - off times
+   *  - distance
+   *  - date
+   */
+}
+
 const page_user_run = (
   user_run_distance_data,
   user_run_interval_data,
