5d5b3ffcd99e04b9583b4b71edfc12183cf66300
nguyenvukhang <brew4k@gmail.com>
2021-07-29 23:44:49 +0800

    spelt out requirements for prettifyIntervals

--- src/utils/prettify-data.js
+++ src/utils/prettify-data.js
@@ -19,6 +19,40 @@ const prettifyDistance = (arr) => {
 }
 
 const prettifyIntervals = (arr) => {
+  /*
+   * expected input: an object
+   *  - Set1: 800m
+   *  - Set2: 800m
+   *  - Set3: 800m
+   *  - Rest1: 1:00
+   *  - Rest2: 1:00
+   *  - Rest3: 1:00
+   *  - Timing1: 2:41
+   *  - Timing2: 2:47
+   *  - Timing3: 2:39
+   *  - Date: 25/07/2021
+   *
+   *  TODO: sync up with Ryan on changing Rest to Interval
+   *        Interval1 = Set 1 Run Time + Set 1 Rest Time
+   *                  = Time from Set 1 start to Set 2 start
+   *
+   *  expected output:
+   *    3 x 800m / 1'      2:41, 2:47, 2:39
+   *
+   * if further sets are added:
+   *  - Set4: 400m
+   *  - Set5: 400m
+   *  - Set6: 400m
+   *  - Rest4: 0:30
+   *  - Rest5: 0:30
+   *  - Rest6: 0:30
+   *  - Timing4: 1:22
+   *  - Timing5: 1:25
+   *  - Timing6: 1:21
+   *
+   *  new expected output:
+   *    3 x 800m / 1', 3 x 400m / 30"          2:41, 2:47, 2:39, 1:22, 1:25, 1:21
+   */
   arr.forEach((training) => {
     const process_date = moment(training.Date, 'DD/MM/YYYY').unix()
     training.SortDate = process_date
