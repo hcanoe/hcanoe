bb3812f5c01814a328e4b5335a5f91b402978a0a
nguyenvukhang <brew4k@gmail.com>
2021-08-05 23:59:28 +0800

    test

--- pages/small.js
+++ pages/small.js
@@ -1,8 +1,11 @@
 import { getDate } from '@utils/date'
 import { allCaps } from '@utils/text'
+import { displayDistance } from '@utils/physics'
 
 const Page = () => {
-  const name = "aiden-ryan"
+  const input = 10000
+  console.log(toString(input))
+  console.log(displayDistance('10000', 'km'))
 
   return (
     null
