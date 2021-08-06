3a9fb07ffd258bbc7fb00a7bfae81cfffe565c7d
nguyenvukhang <brew4k@gmail.com>
2021-08-06 23:34:00 +0800

    made separate component for bests

--- src/components/FieldBox.js
+++ src/components/FieldBox.js
@@ -1,21 +1,29 @@
 import { Text, Box, Heading } from '@chakra-ui/react'
+import styles from 'styles/FieldBox.module.css'
 
 const FieldBox = ({ t, children, c = '#fc4c02' }) => {
   return (
-    <Box borderRadius="lg" shadow="base" pt="0.5em">
+    <Box
+      borderRadius="lg"
+      shadow="base"
+      pt="0.5em"
+      className={styles.field}
+    >
+      {children}
+    </Box>
+  )
+}
+
+const BestBox = ({ t, children, c = '#fc4c02' }) => {
+  return (
+    <Box
+      borderRadius="lg"
+      shadow="base"
+      pt="0.5em"
+    >
       {children}
     </Box>
   )
 }
 export default FieldBox
-
-/*
-      <Text
-        color="#429E90"
-        fontSize="lg"
-        fontWeight="extrabold"
-        mb="0.8em"
-      >
-        {t}
-      </Text>
-*/
+export { BestBox }
