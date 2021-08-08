a8332047ca7801e9d2aa2d2ec2992b4fca49176b
nguyenvukhang <brew4k@gmail.com>
2021-08-08 23:51:08 +0800

    used a builtin chakra ui element for home button

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,24 +1,11 @@
 import { AiOutlineHome } from 'react-icons/ai'
-import { Flex } from '@chakra-ui/react'
+import { Flex, Button } from '@chakra-ui/react'
 
 const HomeButton = () => {
   return (
-    <a href="/">
-      <Flex
-        as="button"
-        h="40px"
-        w="40px"
-        alignItems="center"
-        justifyContent="center"
-        borderRadius="50%"
-        className="noselect"
-        color="teal.350"
-        _hover={{ bg: 'rgba(0, 0, 0, 0.1)' }}
-        _active={{ bg: 'rgba(0, 0, 0, 0.2)' }}
-      >
-        <AiOutlineHome size="20" />
-      </Flex>
-    </a>
+    <Button variant='ghost'>
+      <AiOutlineHome size="20" />
+    </Button>
   )
 }
 
