98078b5acb6566fd8fe0bc01f882f97698e13148
nguyenvukhang <brew4k@gmail.com>
2021-08-18 22:42:44 +0800

    moving functions outta main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,147 +1,22 @@
-import {
-  getUserMetadata,
-  searchUserInDay,
-  getSpreadsheetsByType,
-  zipTable,
-} from '@utils/user-meta'
-import { makeEnglish } from '@utils/text'
-import { getDate } from '@utils/date'
+import { getUserMetadata } from 'utils/user-meta'
+import { getSpreadsheetsByType, getAllSheets, getUserTrainingData } from 'utils/get-data'
+import { makeEnglish } from 'utils/text'
 import {
   prettifyDistance,
   prettifyIntervals,
   prettifyOnOff,
   prettifyTimed,
-} from '@utils/prettify-data'
-import { sheets, query, user_metadata, user_data_by_type } from 'types/types'
+} from 'utils/prettify-data'
+import { sheets, query, user_metadata } from 'types/types'
 
-async function getTrainingData(
-  sheets: sheets,
-  sheetID: string,
-  sheetTitle: string
-) {
-  const range = sheetTitle.concat('!A:Z')
-  const response = await sheets.spreadsheets.values.get({
-    spreadsheetId: sheetID,
-    range,
-  })
-  return response
-}
-
-async function getAllSheets(sheets: sheets, idList: Array<string>) {
-  const result = {}
-  await Promise.all(
-    idList.map(async (id) => {
-      result[id] = {}
-      const response = await sheets.spreadsheets.get({
-        spreadsheetId: id,
-      })
-      const sheetData = response.data.sheets
-      await Promise.all(
-        sheetData.map(async (eachSheetData) => {
-          const title = eachSheetData.properties.title
-          const r = await getTrainingData(sheets, id, title)
-          const trainingData = r.data.values
-          result[id][title] = trainingData
-        })
-      )
-    })
-  )
-
-  return result
-}
-
-const getUserTrainingData = (data_all_sheets: any, name: string) => {
-  const user_data_by_day = {}
-  const user_data_by_type: user_data_by_type = {
-    DISTANCE: [],
-    INTERVALS: [],
-    ONOFF: [],
-    TIMED: [],
-  }
-  console.log('reading data from ->', Object.keys(data_all_sheets))
-  for (const spreadsheet_id in data_all_sheets) {
-    for (const week in data_all_sheets[spreadsheet_id]) {
-      const data_week: Array<string> = data_all_sheets[spreadsheet_id][week]
-      /*
-       * data_week is an array of the week's trainings,
-       * with each day separated by a single cell ['>>>']
-       */
-      const split_day = {}
-      var arr = []
-      data_week.forEach((e, index) => {
-        // const training_id = week + day
-        if (e[0] === '>>>') {
-          // arr[1][0] is the day of week
-          const training_id = arr[1][0] + index
-          split_day[training_id] = arr
-          arr = []
-        } else if (index == data_week.length - 1) {
-          arr.push(e)
-          // arr[1][0] is the day of week
-          const training_id = arr[1][0] + index
-          split_day[training_id] = arr
-        } else {
-          arr.push(e)
-        }
-      })
-      /*
-       * at this point, split_day is an object where each key
-       * contains the entire team's data for that day
-       */
-
-      user_data_by_day[week] = {}
-      for (const day in split_day) {
-        const dayOfWeek = split_day[day][1][0]
-        const date = getDate(week, dayOfWeek)
-        const day_arr = split_day[day]
-        const headers = day_arr.shift()
-        const type = headers.shift()
-        const _body = searchUserInDay(name, day_arr)
-        if (_body) {
-          const body = _body.slice(1)
-          interface zipped {
-            Type?: string
-            Date?: string
-            Name?: string
-          }
-          const zipped: zipped = zipTable(headers, body)
-          zipped.Type = type
-          zipped.Date = date
-          delete zipped.Name
-          user_data_by_type[type].push(zipped)
-          const training_id = week + day + type
-          user_data_by_day[week][training_id] = zipped
-        }
-      }
-
-      /*
-       * at this point, user_data_by_day is an object,
-       *  key = start of week in DD/MM/YYYY format
-       *  props = date of training in DD/MM/YYYY
-       */
-    }
-  }
-  return { by_day: user_data_by_day, by_type: user_data_by_type }
-}
 
 export async function main(query: query, sheets: sheets) {
   const output: any = {}
-  /*
-   * grab year and user from url
-   * hcanoe.vercel.app/18/ning-yiran
-   *   { year: 18, user: ning-yiran }
-   */
   const { year, user } = query
-
-  /*
-   * retrieve user's metadata from google spreadsheet called meta
-   */
   const user_metadata: user_metadata = await getUserMetadata(sheets, user, year)
   const name = user_metadata.Name
 
-  // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
   const spreadsheet_ids_by_type = getSpreadsheetsByType(user_metadata, 'run')
-
   const data_all_sheets = await getAllSheets(sheets, spreadsheet_ids_by_type)
 
   output.log = ['data all sheets', data_all_sheets]
