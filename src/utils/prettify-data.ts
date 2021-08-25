d88985c1b003fc513169645a848905f7ed7b1185
nguyenvukhang <brew4k@gmail.com>
2021-08-25 21:40:44 +0800

    added comments to prettifyOnOff

--- src/utils/prettify-data.ts
+++ src/utils/prettify-data.ts
@@ -10,6 +10,204 @@ import {
 import { recentFirst } from '@utils/sort'
 import { Distance, Intervals, OnOff, Timed } from 'types/types'
 
+namespace sets {
+  export type Intervals = Array<{
+    set?: string
+    rest?: string
+    timing?: string
+  }>
+  export type OnOff = Array<{
+    off?: string
+    on?: string
+  }>
+}
+
+namespace programme {
+  /*
+   * Reads an array of objects, each object representing one interval set
+   *
+   * returns three arrays of strings: Programme, Timings, Paces.
+   * The length of these arrays will be the same.
+   * The strings within these arrays have been formatted for reading.
+   * intented to merge into an interval training as props
+   */
+  export function intervals(d: sets.Intervals) {
+    const programme = []
+    const timings = []
+    const paces = []
+    var c = 0
+    type Mem = {
+      set?: string // previous set's distance
+      rest?: string // previous set's rest time
+      timings?: Array<number> // previous sets' timings
+    }
+    var mem: Mem = {}
+    function toPaces(timings: Array<number>, distance: string) {
+      paces.push(
+        displayPace(
+          timings.reduce((a, b) => a + b, 0), // total timing in seconds
+          toMeters(distance) * c // total distance in meters
+        )
+      )
+    }
+    function toProgramme(c: number, distance: string, rest: string) {
+      programme.push(c + 'x' + distance + '/' + quoteNotation(rest))
+    }
+    d.forEach((e, index) => {
+      function same(type: string) {
+        return d[index][type] === d[index - 1][type] ? true : false
+      }
+      if (index === 0) {
+        // first element
+        c += 1
+        mem = { ...mem, set: e.set, rest: e.rest }
+        mem.timings = [toSeconds(e.timing)]
+      } else if (index === d.length - 1) {
+        // last element
+        if (same('set') && same('rest')) {
+          c += 1
+          toProgramme(c, mem.set, mem.rest)
+          mem.timings.push(toSeconds(e.timing))
+          timings.push(mem.timings.map((x) => toHHMMSS(x)))
+          toPaces(mem.timings, mem.set)
+        } else {
+          toProgramme(c, mem.set, mem.rest)
+          timings.push(mem.timings.map((x) => toHHMMSS(x)))
+          toPaces(mem.timings, mem.set)
+          programme.push(1 + 'x' + e.set + '/' + quoteNotation(e.rest))
+          timings.push([toHHMMSS(e.timing)])
+          paces.push(displayPace(toSeconds(e.timing), toMeters(e.set)))
+        }
+      } else {
+        // neither first nor last element
+        if (same('set') && same('rest')) {
+          c += 1
+          mem.timings.push(toSeconds(e.timing))
+        } else {
+          toProgramme(c, mem.set, mem.rest)
+          timings.push(mem.timings.map((x) => toHHMMSS(x)))
+          toPaces(mem.timings, mem.set)
+          mem = { ...mem, set: e.set, rest: e.rest }
+          mem.timings = [toSeconds(e.timing)] // reset Timings in memory
+        }
+      }
+    })
+    return {
+      programme,
+      timings: timings.map((x) => x.join(', ')),
+      paces,
+    }
+  }
+  export function onOff(d: sets.OnOff) {
+    const programme = []
+    var c = 0
+    var mem = {
+      on: '',
+      off: '',
+      dash: [],
+      streak: false,
+    }
+    d.forEach((e, index) => {
+      e.on = quoteNotation(e.on)
+      e.off = quoteNotation(e.off)
+      /*
+       * e contains an object with on and off props
+       */
+      const sameOn = () => {
+        return d[index].on === d[index - 1].on ? true : false
+      }
+      const sameOff = () => {
+        return d[index].off === d[index - 1].off ? true : false
+      }
+      const pushSet = () => {
+        if (c === 1) {
+          programme.push(mem.on + '/' + mem.off)
+        } else {
+          programme.push(c + 'x' + mem.on + '/' + mem.off)
+        }
+      }
+      /*
+       * expected output:
+       *   7x7'/1'
+       *   3x7'/1', 3x3'/1'
+       *   7'-6'-5'-4'-3'-2'-1'/1'
+       */
+      if (index === 0) {
+        mem.off = e.off
+        mem.on = e.on
+        c += 1
+      } else if (index === d.length - 1) {
+        // last element
+        if (sameOff()) {
+          if (sameOn()) {
+            c += 1
+            programme.push(c + 'x' + mem.on + '/' + mem.off)
+          } else if (!sameOn()) {
+            if (mem.streak) {
+              // dash train has started
+              mem.dash.push(e.on)
+              programme.push(mem.dash.join('-') + '/' + mem.off)
+            } else if (c == 1) {
+              // no dash train yet (same off, diff on)
+              mem.dash = [mem.on, e.on]
+              programme.push(mem.dash.join('-') + '/' + mem.off)
+            } else {
+              pushSet()
+              programme.push(e.on + '/' + e.off)
+            }
+          }
+        } else if (!sameOff()) {
+          if (mem.streak) {
+            // push dash train to programme
+            programme.push(mem.dash.join('-') + '/' + mem.off)
+          } else {
+            pushSet()
+          }
+          mem.on = e.on
+          mem.off = e.off
+          programme.push(mem.on + '/' + mem.off)
+        }
+      } else {
+        // in-between
+        if (sameOff()) {
+          if (sameOn()) {
+            c += 1
+          } else if (!sameOn()) {
+            if (mem.streak) {
+              // dash train has started
+              mem.dash.push(e.on)
+            } else if (c == 1) {
+              // no dash train yet (same off, diff on)
+              // and set count is 1
+              mem.dash = [mem.on, e.on]
+              mem.streak = true
+            } else {
+              pushSet()
+              mem.on = e.on
+              c = 1
+            }
+          }
+        } else if (!sameOff()) {
+          if (mem.streak) {
+            // push dash train to programme
+            const tempStreak = mem.dash.join('-')
+            programme.push(tempStreak + '/' + mem.off)
+          } else {
+            pushSet()
+          }
+          mem.on = e.on
+          mem.off = e.off
+          mem.streak = false
+          mem.dash = []
+        }
+      }
+    })
+    const output: any = {}
+    output.programme = programme.join(', ')
+    return output
+  }
+}
+
 /* Prettify Distance */
 function prettifyDistance(arr: Distance) {
   const cat = [1000, 2400, 4000, 5000, 7000, 10000, 15000, 21000, 42195, 50000]
@@ -66,11 +264,6 @@ function prettifyDistance(arr: Distance) {
 }
 
 /* Prettify Intervals */
-type IntervalSets = Array<{
-  set?: string
-  rest?: string
-  timing?: string
-}>
 function prettifyIntervals(arr: Intervals) {
   const isKeyword = {
     set: 1,
@@ -79,7 +272,7 @@ function prettifyIntervals(arr: Intervals) {
   }
   /* arr is an array with each element representing one training session */
   arr.forEach((training) => {
-    const sets: IntervalSets = []
+    const sets: sets.Intervals = []
     for (const key in training) {
       /* Some headers are expected to have numbers, denoting
        * the set's order
@@ -92,206 +285,17 @@ function prettifyIntervals(arr: Intervals) {
       }
     }
     /* sets is now an array of objects,
-     * each object containing the Set (Distance), Rest, and Timing values for
+     * each object containing the set (distance), rest, and timing values for
      * that set
      */
-    Object.assign(training, intervalsProgramme(sets))
+    Object.assign(training, programme.intervals(sets))
     training.sortDate = moment(training.Date, 'DD/MM/YYYY').unix()
   })
   arr.sort(recentFirst)
   return arr
 }
 
-/*
- * Reads an array of objects, each object representing one interval set
- *
- * returns three arrays of strings: Programme, Timings, Paces.
- * The length of these arrays will be the same.
- * The strings within these arrays have been formatted for reading.
- * intented to merge into an interval training as props
- */
-function intervalsProgramme(d: IntervalSets) {
-  const programme = []
-  const timings = []
-  const paces = []
-  var c = 0
-  type Mem = {
-    set?: string // previous set's distance
-    rest?: string // previous set's rest time
-    timings?: Array<number> // previous sets' timings
-  }
-  var mem: Mem = {}
-  function toPaces(timings: Array<number>, distance: string) {
-    paces.push(
-      displayPace(
-        timings.reduce((a, b) => a + b, 0), // total timing in seconds
-        toMeters(distance) * c // total distance in meters
-      )
-    )
-  }
-  function toProgramme(c: number, distance: string, rest: string) {
-    programme.push(c + 'x' + distance + '/' + quoteNotation(rest))
-  }
-  d.forEach((e, index) => {
-    function same(type: string) {
-      return d[index][type] === d[index - 1][type] ? true : false
-    }
-    if (index === 0) {
-      // first element
-      c += 1
-      mem = { ...mem, set: e.set, rest: e.rest }
-      mem.timings = [toSeconds(e.timing)]
-    } else if (index === d.length - 1) {
-      // last element
-      if (same('set') && same('rest')) {
-        c += 1
-        toProgramme(c, mem.set, mem.rest)
-        mem.timings.push(toSeconds(e.timing))
-        timings.push(mem.timings.map((x) => toHHMMSS(x)))
-        toPaces(mem.timings, mem.set)
-      } else {
-        toProgramme(c, mem.set, mem.rest)
-        timings.push(mem.timings.map((x) => toHHMMSS(x)))
-        toPaces(mem.timings, mem.set)
-        programme.push(1 + 'x' + e.set + '/' + quoteNotation(e.rest))
-        timings.push([toHHMMSS(e.timing)])
-        paces.push(displayPace(toSeconds(e.timing), toMeters(e.set)))
-      }
-    } else {
-      // neither first nor last element
-      if (same('set') && same('rest')) {
-        c += 1
-        mem.timings.push(toSeconds(e.timing))
-      } else {
-        toProgramme(c, mem.set, mem.rest)
-        timings.push(mem.timings.map((x) => toHHMMSS(x)))
-        toPaces(mem.timings, mem.set)
-        mem = { ...mem, set: e.set, rest: e.rest }
-        mem.timings = [toSeconds(e.timing)] // reset Timings in memory
-      }
-    }
-  })
-  return {
-    programme,
-    timings: timings.map((x) => x.join(', ')),
-    paces,
-  }
-}
-
-const onOffProgramme = (d: onOffSets) => {
-  const programme = []
-  var c = 0
-  var mem = {
-    on: '',
-    off: '',
-    dash: [],
-    streak: false,
-  }
-  d.forEach((e, index) => {
-    e.on = quoteNotation(e.on)
-    e.off = quoteNotation(e.off)
-    /*
-     * e contains an object with on and off props
-     */
-    const sameOn = () => {
-      return d[index].on === d[index - 1].on ? true : false
-    }
-    const sameOff = () => {
-      return d[index].off === d[index - 1].off ? true : false
-    }
-    const pushSet = () => {
-      if (c === 1) {
-        programme.push(mem.on + '/' + mem.off)
-      } else {
-        programme.push(c + 'x' + mem.on + '/' + mem.off)
-      }
-    }
-    /*
-     * expected output:
-     *   7x7'/1'
-     *   3x7'/1', 3x3'/1'
-     *   7'-6'-5'-4'-3'-2'-1'/1'
-     */
-    if (index === 0) {
-      mem.off = e.off
-      mem.on = e.on
-      c += 1
-    } else if (index === d.length - 1) {
-      // last element
-      if (sameOff()) {
-        if (sameOn()) {
-          c += 1
-          programme.push(c + 'x' + mem.on + '/' + mem.off)
-        } else if (!sameOn()) {
-          if (mem.streak) {
-            // dash train has started
-            mem.dash.push(e.on)
-            programme.push(mem.dash.join('-') + '/' + mem.off)
-          } else if (c == 1) {
-            // no dash train yet (same off, diff on)
-            mem.dash = [mem.on, e.on]
-            programme.push(mem.dash.join('-') + '/' + mem.off)
-          } else {
-            pushSet()
-            programme.push(e.on + '/' + e.off)
-          }
-        }
-      } else if (!sameOff()) {
-        if (mem.streak) {
-          // push dash train to programme
-          programme.push(mem.dash.join('-') + '/' + mem.off)
-        } else {
-          pushSet()
-        }
-        mem.on = e.on
-        mem.off = e.off
-        programme.push(mem.on + '/' + mem.off)
-      }
-    } else {
-      // in-between
-      if (sameOff()) {
-        if (sameOn()) {
-          c += 1
-        } else if (!sameOn()) {
-          if (mem.streak) {
-            // dash train has started
-            mem.dash.push(e.on)
-          } else if (c == 1) {
-            // no dash train yet (same off, diff on)
-            // and set count is 1
-            mem.dash = [mem.on, e.on]
-            mem.streak = true
-          } else {
-            pushSet()
-            mem.on = e.on
-            c = 1
-          }
-        }
-      } else if (!sameOff()) {
-        if (mem.streak) {
-          // push dash train to programme
-          const tempStreak = mem.dash.join('-')
-          programme.push(tempStreak + '/' + mem.off)
-        } else {
-          pushSet()
-        }
-        mem.on = e.on
-        mem.off = e.off
-        mem.streak = false
-        mem.dash = []
-      }
-    }
-  })
-  const output: any = {}
-  output.programme = programme.join(', ')
-  return output
-}
-
 /* Prettify OnOff */
-type onOffSets = Array<{
-  off?: string
-  on?: string
-}>
 const prettifyOnOff = (arr: OnOff) => {
   const isKeyword = {
     on: 1,
@@ -299,7 +303,7 @@ const prettifyOnOff = (arr: OnOff) => {
   }
   arr.forEach((training) => {
     // training is an object
-    const sets: onOffSets = [] // each set being { on: "", off: "" }
+    const sets: sets.OnOff = [] // each set being { on: "", off: "" }
     for (const key in training) {
       /* Some headers are expected to have numbers, denoting
        * the set's order
@@ -311,10 +315,12 @@ const prettifyOnOff = (arr: OnOff) => {
         sets[order][heading] = training[key]
       }
     }
-    Object.assign(training, onOffProgramme(sets))
+    /* sets is now an array of objects,
+     * each object containing the on and off values for each set
+     */
+    Object.assign(training, programme.onOff(sets))
     training.distance = displayDistance(training.distance, 'km')
-    const processDate = moment(training.date, 'DD/MM/YYYY').unix()
-    training.sortDate = processDate
+    training.sortDate = moment(training.date, 'DD/MM/YYYY').unix()
   })
   arr.sort(recentFirst)
   return arr
